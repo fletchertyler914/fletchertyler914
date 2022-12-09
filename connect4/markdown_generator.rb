a9b58da47ba09e2df01e49927ab6913e71eb8b3c
Tyler Fletcher <fletchertyler914@yahoo.com>
2022-12-09 01:24:21 -0600

    markdown updates

--- connect4/markdown_generator.rb
+++ connect4/markdown_generator.rb
@@ -21,94 +21,90 @@ class MarkdownGenerator
     game_winning_players = metadata[:game_winning_players].sort_by { |_, wins| -wins }
 
     markdown = <<~HTML
-    <h2 align="center">
-      <img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="35">
-      <b>Hey, I'm Tyler Fletcher</b>
+      <h1 align="center">
+              <img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="35">
+              <b>Hey, I'm Tyler Fletcher</b>
+            </h1>
 
-      ---
+        <p align="center">
+          <a href="https://github.com/DenverCoder1/readme-typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=Time+New+Roman&color=cyan&size=25&center=true&vCenter=true&width=600&height=100&lines=Tyler+Fletcher;self-taught-full-stack;solana<3;terminally-curious;oss<3"></a>
+        </p>
 
-      </h2>
-
-      <p align="center">
-        <a href="https://github.com/DenverCoder1/readme-typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=Time+New+Roman&color=cyan&size=25&center=true&vCenter=true&width=600&height=100&lines=Tyler+Fletcher;self-taught-full-stack;solana<3;terminally-curious;oss<3"></a>
-      </p>
-
-      <br>
-
-    ## <picture><img src = "https://github.com/0xAbdulKhalid/0xAbdulKhalid/raw/main/assets/mdImages/about_me.gif" width = 50px></picture> **About me**
-
-    <picture> <img align="right" src="https://github.com/0xAbdulKhalid/0xAbdulKhalid/raw/main/assets/mdImages/Right_Side.gif" width = 250px></picture>
-
-      <br>
-
-    - A terminally-curious self-taught full-stack developer
-    - Building on [Solana](https://solana.com) full-time with [NATION](https://nation.io)
-    - Currently learning [anchor](https://www.anchor-lang.com/)/rust, and looking into [qwik](https://qwik.builder.io/)
-    - [Personal website](https://tylerjfletcher.xyz) (under construction/outdated :p)
-    - 3 kids that drive me insane in the best way
-
-    <br><br><br><br><br>
-
-    ---
-
-      <br>
-
-    ## <img src="https://media.giphy.com/media/iY8CRBdQXODJSCERIr/giphy.gif" width="35"><b> Github Stats </b>
-
-      <br>
-
-      <div align="center">
-
-      <a href="https://github.com/fletchertyler914/">
         <br>
-        <img src="https://github-readme-stats.vercel.app/api?username=fletchertyler914&include_all_commits=true&count_private=true&show_icons=true&line_height=20&title_color=7A7ADB&icon_color=2234AE&text_color=D3D3D3&bg_color=0,000000,130F40" width="565"/>
-        <br><br><br>
-        <img src="https://github-readme-stats.vercel.app/api/top-langs?username=fletchertyler914&show_icons=true&locale=en&layout=compact&line_height=20&title_color=7A7ADB&icon_color=2234AE&text_color=D3D3D3&bg_color=0,000000,130F40" width="400"  alt="fletchertyler914"/>
 
-      </a>
+      <h2 align='center'> <picture><img src = "https://github.com/0xAbdulKhalid/0xAbdulKhalid/raw/main/assets/mdImages/about_me.gif" width = 50px></picture> <b>About me</b></h2>
 
-    <br><br>
+      <picture> <img align="right" src="https://github.com/0xAbdulKhalid/0xAbdulKhalid/raw/main/assets/mdImages/Right_Side.gif" width = 250px></picture>
 
-    ![](https://komarev.com/ghpvc/?username=fletchertyler914&color=blue)
+        <br>
+
+      - A terminally-curious self-taught full-stack developer
+      - Building on [Solana](https://solana.com) full-time with [NATION](https://nation.io)
+      - Currently learning [anchor](https://www.anchor-lang.com/)/rust, and looking into [qwik](https://qwik.builder.io/)
+      - [Personal website](https://tylerjfletcher.xyz) (under construction/outdated :p)
+      - 3 kids that drive me insane in the best way
+
+      <br><br><br>
+
+      <h2 align='center'><img src="https://media.giphy.com/media/iY8CRBdQXODJSCERIr/giphy.gif" width="35"><b> Github Stats </b></h2>
+
+        <br>
+
+        <div align="center">
+
+        <a href="https://github.com/fletchertyler914/">
+          <br>
+          <img src="https://github-readme-stats.vercel.app/api?username=fletchertyler914&include_all_commits=true&count_private=true&show_icons=true&line_height=20&title_color=7A7ADB&icon_color=2234AE&text_color=D3D3D3&bg_color=0,000000,130F40" width="565"/>
+          <br><br><br>
+          <img src="https://github-readme-stats.vercel.app/api/top-langs?username=fletchertyler914&show_icons=true&locale=en&layout=compact&line_height=20&title_color=7A7ADB&icon_color=2234AE&text_color=D3D3D3&bg_color=0,000000,130F40" width="400"  alt="fletchertyler914"/>
+
+        </a>
+
+      <br><br>
+
+      <h2 align='center'> <img src="https://github.com/0xAbdulKhalid/0xAbdulKhalid/raw/main/assets/mdImages/handshake.gif" width ="80"><br><b>Connect</b></h2>
+
+        <a href="https://www.linkedin.com/in/tyler-fletcher/" target="_blank">
+        <img src="https://img.shields.io/badge/linkedin:  tyler--fletcher-%2300acee.svg?color=405DE6&style=for-the-badge&logo=linkedin&logoColor=white" alt=linkedin style="margin-bottom: 5px;"/>
+        </a>
+
+        <br>
+
+        <a href="https://twitter.com/firecrab_" target="_blank">
+        <img src="https://img.shields.io/badge/twitter:  firecrab__-%2300acee.svg?color=1DA1F2&style=for-the-badge&logo=twitter&logoColor=white" alt=twitter style="margin-bottom: 5px;"/>
+        </a>
+        </li>
+
+        <br>
+
+        <a href="mailto:hello@tylerjfletcher.xyz" target="_blank">
+        <img src="https://img.shields.io/badge/proton:  tylerjfletcher.xyz-6D4AFF.svg?style=for-the-badge&logo=protonmail&logoColor=white" t=mail style="margin-bottom: 5px;" />
+        </a>
+
+      <br><br>
+
+      <img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif">
 
       <br>
 
-    ---
+      <b> :game_die: Join my community Connect Four game! <b>
 
-    <br><br>
+      ![](https://img.shields.io/badge/Moves%20played-2-blue)
+      ![](https://img.shields.io/badge/Completed%20games-0-brightgreen)
+      ![](https://img.shields.io/badge/Total%20players-1-orange)
 
-    ## <img src="https://github.com/0xAbdulKhalid/0xAbdulKhalid/raw/main/assets/mdImages/handshake.gif" width ="80"><br><b>Connect</b>
-
+      Everyone is welcome to participate! To make a move, click on the **column number** you wish to drop your disk in.
       <br>
 
-      <a href="https://www.linkedin.com/in/tyler-fletcher/" target="_blank">
-      <img src="https://img.shields.io/badge/linkedin:  tyler--fletcher-%2300acee.svg?color=405DE6&style=for-the-badge&logo=linkedin&logoColor=white" alt=linkedin style="margin-bottom: 5px;"/>
-      </a>
+      <img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif">
 
       <br>
-
-      <a href="https://twitter.com/firecrab_" target="_blank">
-      <img src="https://img.shields.io/badge/twitter:  firecrab__-%2300acee.svg?color=1DA1F2&style=for-the-badge&logo=twitter&logoColor=white" alt=twitter style="margin-bottom: 5px;"/>
-      </a>
-      </li>
-
-      <br>
-
-      <a href="mailto:hello@tylerjfletcher.xyz" target="_blank">
-      <img src="https://img.shields.io/badge/proton:  tylerjfletcher.xyz-6D4AFF.svg?style=for-the-badge&logo=protonmail&logoColor=white" t=mail style="margin-bottom: 5px;" />
-      </a>
-
-      <br>
-      <br>
-
-    <img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif"><br>
-
     HTML
 
     game_status = if game.over?
-      "\n#{game.status_string} [Click here to start a new game!](#{ISSUE_BASE_URL}?title=connect4%7Cnew&#{ISSUE_BODY})\n\n"
+      "#{game.status_string} [Click here to start a new game!](#{ISSUE_BASE_URL}?title=connect4%7Cnew&#{ISSUE_BODY})\n\n"
     else
-      "\nIt is the **#{current_turn}** team's turn to play.\n\n"
+      "It is the **#{current_turn}** team's turn to play.\n\n"
     end
 
     markdown.concat("#{game_status}\n\n")
@@ -117,15 +113,12 @@ class MarkdownGenerator
 
     unless game.over?
           markdown.concat <<~HTML
+          
+          ---
 
-              <br><br>
-
-              ---
-              Tired of waiting?<br>
-              [Request an AI move](#{ISSUE_BASE_URL}?title=connect4%7Cdrop%7C#{current_turn}%7Cai&#{ISSUE_BODY})
+          Tired of waiting?<br>
+          [Request an AI move](#{ISSUE_BASE_URL}?title=connect4%7Cdrop%7C#{current_turn}%7Cai&#{ISSUE_BODY})
           HTML
-
-      markdown.concat("\n\nTired of waiting?\n[Request an AI move](#{ISSUE_BASE_URL}?title=connect4%7Cdrop%7C#{current_turn}%7Cai&#{ISSUE_BODY})")
     end
 
     markdown.concat <<~HTML
