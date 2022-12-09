5a9acf141621abfd0fc90dfbf5940275901d12ad
Tyler Fletcher <fletchertyler914@yahoo.com>
2022-12-08 23:20:05 -0600

    Fix markdown generatort

--- README.md
+++ README.md
@@ -43,16 +43,16 @@
   <img src="https://github-readme-stats.vercel.app/api/top-langs?username=fletchertyler914&show_icons=true&locale=en&layout=compact&line_height=20&title_color=7A7ADB&icon_color=2234AE&text_color=D3D3D3&bg_color=0,000000,130F40" width="400"  alt="fletchertyler914"/>
 
 </a>
-</div>
 
 <br><br><br>
-<img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif">
+
+---
+
 <br><br>
 
-## <img src="https://github.com/0xAbdulKhalid/0xAbdulKhalid/raw/main/assets/mdImages/handshake.gif" width ="80"><b>Connect</b>
+## <img src="https://github.com/0xAbdulKhalid/0xAbdulKhalid/raw/main/assets/mdImages/handshake.gif" width ="80"><br><b>Connect</b>
 
 <br>
-<div align='left'>
 
 <a href="https://www.linkedin.com/in/tyler-fletcher/" target="_blank">
 <img src="https://img.shields.io/badge/linkedin:  tyler--fletcher-%2300acee.svg?color=405DE6&style=for-the-badge&logo=linkedin&logoColor=white" alt=linkedin style="margin-bottom: 5px;"/>
@@ -71,15 +71,11 @@
 <img src="https://img.shields.io/badge/proton:  tylerjfletcher.xyz-6D4AFF.svg?style=for-the-badge&logo=protonmail&logoColor=white" t=mail style="margin-bottom: 5px;" />
 </a>
 
-</div>
- 
 <br>
-<br>
-
----
-
 <br>
 
 Profile inspired by: [Abdul Khalid](https://github.com/0xabdulkhalid)
 
 Last Edited on: December 8th, 2022
+
+</div>
--- connect4/markdown_generator.rb
+++ connect4/markdown_generator.rb
@@ -1,8 +1,8 @@
 require_relative './game'
 
 class MarkdownGenerator
-  IMAGE_BASE_URL = 'https://raw.githubusercontent.com/JonathanGin52/JonathanGin52/main/images'
-  ISSUE_BASE_URL = 'https://github.com/JonathanGin52/JonathanGin52/issues/new'
+  IMAGE_BASE_URL = 'https://raw.githubusercontent.com/fletchertyler914/fletchertyler914/main/images'
+  ISSUE_BASE_URL = 'https://github.com/fletchertyler914/fletchertyler914/issues/new'
   ISSUE_BODY = 'body=Just+push+%27Submit+new+issue%27+without+editing+the+title.+The+README+will+be+updated+after+approximately+30+seconds.'
 
   RED_IMAGE = "![](#{IMAGE_BASE_URL}/red.png)"
@@ -21,20 +21,99 @@ class MarkdownGenerator
     game_winning_players = metadata[:game_winning_players].sort_by { |_, wins| -wins }
 
     markdown = <<~HTML
-        # Hey, I'm Jonathan 👋
+      <h1 align="center">
+        <img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="35">
+        <b>Hey, I'm Tyler Fletcher</b>
 
-        [![Twitter Badge](https://img.shields.io/badge/-@JonathanGin52-1ca0f1?style=flat-square&labelColor=1ca0f1&logo=twitter&logoColor=white&link=https://twitter.com/jonathangin52)](https://twitter.com/jonathangin52) [![Linkedin Badge](https://img.shields.io/badge/-JonathanGin-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/jonathangin/)](https://www.linkedin.com/in/jonathangin/)
+        ![](https://komarev.com/ghpvc/?username=fletchertyler914&color=blue)
 
-        Nice to meet you! I'm currently working at GitHub as a software engineer.
-        I recently graduated from the [Shopify Dev Degree](https://devdegree.ca/) program in Spring 2022.
-        I've previously worked on building the [Shopify Fulfillment Network](https://www.shopify.com/fulfillment) as a fullstack developer, Shopify's Experimentation Platform as a data developer, Shopify Checkout as a backend developer, and Shopify's Caching Platform as a production engineer.
+        </h1>
+
+        <p align="center">
+          <a href="https://github.com/DenverCoder1/readme-typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=Time+New+Roman&color=cyan&size=25&center=true&vCenter=true&width=600&height=100&lines=Tyler+Fletcher;self-taught-full-stack;solana<3;terminally-curious;oss<3"></a>
+        </p>
+
+        <br>
+
+        ## <picture><img src = "https://github.com/0xAbdulKhalid/0xAbdulKhalid/raw/main/assets/mdImages/about_me.gif" width = 50px></picture> **About me**
+
+        <picture> <img align="right" src="https://github.com/0xAbdulKhalid/0xAbdulKhalid/raw/main/assets/mdImages/Right_Side.gif" width = 250px></picture>
+
+        <br>
+
+        - A terminally-curious self-taught full-stack developer
+        - Building on [Solana](https://solana.com) full-time with [NATION](https://nation.io)
+        - Currently learning [anchor](https://www.anchor-lang.com/)/rust, and looking into [qwik](https://qwik.builder.io/)
+        - [Personal website](https://tylerjfletcher.xyz) (under construction/outdated :p)
+        - 3 kids that drive me insane in the best way
+
+        <br><br>
+
+        ---
+
+        <br>
+
+        ## <img src="https://media.giphy.com/media/iY8CRBdQXODJSCERIr/giphy.gif" width="35"><b> Github Stats </b>
+
+        <br>
+
+        <div align="center">
+
+        <a href="https://github.com/fletchertyler914/">
+          <br>
+          <img src="https://github-readme-stats.vercel.app/api?username=fletchertyler914&include_all_commits=true&count_private=true&show_icons=true&line_height=20&title_color=7A7ADB&icon_color=2234AE&text_color=D3D3D3&bg_color=0,000000,130F40" width="565"/>
+          <br><br><br>
+          <img src="https://github-readme-stats.vercel.app/api/top-langs?username=fletchertyler914&show_icons=true&locale=en&layout=compact&line_height=20&title_color=7A7ADB&icon_color=2234AE&text_color=D3D3D3&bg_color=0,000000,130F40" width="400"  alt="fletchertyler914"/>
+
+        </a>
+
+        <br><br><br>
+
+        ---
+
+        <br><br>
+
+        ## <img src="https://github.com/0xAbdulKhalid/0xAbdulKhalid/raw/main/assets/mdImages/handshake.gif" width ="80"><br><b>Connect</b>
+
+        <br>
+
+        <a href="https://www.linkedin.com/in/tyler-fletcher/" target="_blank">
+        <img src="https://img.shields.io/badge/linkedin:  tyler--fletcher-%2300acee.svg?color=405DE6&style=for-the-badge&logo=linkedin&logoColor=white" alt=linkedin style="margin-bottom: 5px;"/>
+        </a>
+
+        <br>
+
+        <a href="https://twitter.com/firecrab_" target="_blank">
+        <img src="https://img.shields.io/badge/twitter:  firecrab__-%2300acee.svg?color=1DA1F2&style=for-the-badge&logo=twitter&logoColor=white" alt=twitter style="margin-bottom: 5px;"/>
+        </a>
+        </li>
+
+        <br>
+
+        <a href="mailto:hello@tylerjfletcher.xyz" target="_blank">
+        <img src="https://img.shields.io/badge/proton:  tylerjfletcher.xyz-6D4AFF.svg?style=for-the-badge&logo=protonmail&logoColor=white" t=mail style="margin-bottom: 5px;" />
+        </a>
+
+        <br>
+        <br>
+
+        <img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif"><br>
 
         ## :game_die: Join my community Connect Four game!
+
         ![](https://img.shields.io/badge/Moves%20played-#{total_moves_played}-blue)
         ![](https://img.shields.io/badge/Completed%20games-#{completed_games}-brightgreen)
         ![](https://img.shields.io/badge/Total%20players-#{metadata[:all_players].size}-orange)
 
         Everyone is welcome to participate! To make a move, click on the **column number** you wish to drop your disk in.
+        <br>
+
+        <img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif">
+
+        <br><br>
+
+        </div>
+
 
     HTML
 
@@ -54,7 +133,7 @@ class MarkdownGenerator
 
     markdown.concat <<~HTML
 
-        Interested in how everything works? [Click here](https://github.com/JonathanGin52/JonathanGin52/tree/main/connect4) to read up on what's happening behind the scenes.
+        Interested in how everything works? [Click here](https://github.com/fletchertyler914/fletchertyler914/tree/main/connect4) to read up on what's happening behind the scenes.
 
         **:alarm_clock: Most recent moves**
         | Team | Move | Made by |
@@ -106,6 +185,12 @@ class MarkdownGenerator
 
       ### Blue team roster
       #{generate_player_moves_table(blue_team)}
+
+      <br><br>
+
+      Profile inspired by: [0xabdulkhalid](https://github.com/0xabdulkhalid) and [JonathanGin52](https://github.com/JonathanGin52)
+
+      Last Edited on: December 8th, 2022
     HTML
   end
 
